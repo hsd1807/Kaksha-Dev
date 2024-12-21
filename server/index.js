63047ed3f1c592cbbf73bd98076d9337be6cc49f
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-12-21 17:56:40 +0530

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -4,13 +4,19 @@ import { connectDb } from './database/db.js';
 
 dotenv.config();
 
-const app = express();
+const app = express()
 
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
      res.send("Server is Working");
 });
+
+// Importing Routes
+import userRoutes from './routes/user.js';
+
+// Using Routes
+app.use('/api', userRoutes);
 
 app.listen(port, () => {
     console.log(`Server is runnig on http://localhost:${port}`);
