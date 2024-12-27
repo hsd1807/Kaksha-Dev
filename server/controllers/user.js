7b2f10a64a5fcaf812cd474d0c5bf50c719f84d1
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-12-27 21:07:13 +0530

    Updated controller user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,53 @@
+import { User } from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import sendMail from "../middlewares/sendMail.js";
+
 export const register = async(req, res) => {
     try {
-        res.send("Register API");
+        const {email, name, password} = req.body;
+
+        let user = await User.findOne({email});
+
+        if (user)
+            return res.status(400).json({
+            message: "User Already exists",
+        });
+
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        user = {
+            name,
+            email,
+            password: hashPassword
+        }
+
+        const otp = Math.floor(Math.random() * 1000000);
+
+        const activationToken = jwt.sign(
+        {
+            user,
+            otp,
+        }, process.env.Activation_Secret,
+    {
+        expiresIn: "5m",
+    }
+    );
+
+    const data = {
+        name, otp
+    };
+
+    await sendMail(
+        email,
+        "KakshaDev",
+        data
+    )
+
+    res.status(200).json({
+        message: "OTP sent to your Mail",
+        activationToken,
+    })
     } catch (error) {
         res.status(500).json({
             message: error.message,
