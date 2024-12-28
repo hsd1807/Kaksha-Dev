cc1d60e6f7e219ce0ce16dbb324ee1c9bd93e9b3
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-12-28 21:20:47 +0530

    Updated admin.controller.js

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,5 +1,6 @@
 import tryCatch from "../middlewares/tryCatch.js";
 import { Course } from "../models/course.js";
+import { Lecture } from "../models/lecture.js";
 
 export const createCourse = tryCatch(async(req, res) => {
     const {title, description, category, createdBy, duration, price} = req.body;
@@ -18,5 +19,29 @@ export const createCourse = tryCatch(async(req, res) => {
 
     res.status(201).json({
         message: "Course created successfully",
+    });
+});
+
+export const addLecture = tryCatch(async(req,res) => {
+    const course = await Course.findById(req.params.id);
+
+    if (!course) return res.status(404).json({
+        message: "No Course with this ID",
+    });
+
+    const {title, description} = req.body;
+
+    const file = req.file;
+
+    const lecture = await Lecture.create({
+        title,
+        description,
+        video: file?.path,
+        course: course._id,
+    });
+
+    res.status(201).json({
+        message: "Lecture Added",
+        lecture,
     });
 });
