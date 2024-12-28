76552b36c8d981f538d4b42f6af1e0dd3d9e15e1
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-12-28 20:53:40 +0530

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -15,11 +15,17 @@ app.get('/', (req, res) => {
      res.send("Server is Working");
 });
 
+app.use("/uploads", express.static("uploads"));
+
 // Importing Routes
 import userRoutes from './routes/user.js';
+import courseRoutes from './routes/course.js';
+import adminRoutes from './routes/admin.js';
 
 // Using Routes
 app.use('/api', userRoutes);
+app.use('/api', courseRoutes);
+app.use('/api', adminRoutes);
 
 app.listen(port, () => {
     console.log(`Server is runnig on http://localhost:${port}`);
