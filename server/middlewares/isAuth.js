550517767ad4536be5b25fe49d1bb6827f7f10ba
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-12-28 20:55:45 +0530

    Updated isAuth.js

--- server/middlewares/isAuth.js
+++ server/middlewares/isAuth.js
@@ -20,3 +20,17 @@ export const isAuth = async(req, res, next) => {
         });
     }
 };
+
+export const isAdmin = (req, res, next) => {
+    try {
+        if (req.user.role !== "admin") return res.status(403).json({
+            message: "You are not Admin",
+        });
+
+        next();
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+}
