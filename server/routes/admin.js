cdc0871b09605516f389a80cb46cadeff4a690dd
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-12-28 21:19:54 +0530

    Updated admin.routes.js

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { isAdmin, isAuth } from '../middlewares/isAuth.js';
-import { createCourse } from '../controllers/admin.js';
+import { addLecture, createCourse } from '../controllers/admin.js';
 import { uploadFiles } from '../middlewares/multer.js';
 
 const router = express.Router();
 
 router.post('/course/new', isAuth, isAdmin  , uploadFiles, createCourse);
-
+router.post('/course/:id', isAuth, isAdmin, uploadFiles, addLecture)
 export default router;
