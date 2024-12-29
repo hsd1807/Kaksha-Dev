5be879d36a5faadb9c411a8b0f0f6761dca2af48
hsd1807 <139839711+hsd1807@users.noreply.github.com>
2024-12-29 14:45:58 +0530

    Updated admin.controller.js

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,6 +1,11 @@
 import tryCatch from "../middlewares/tryCatch.js";
 import { Course } from "../models/course.js";
 import { Lecture } from "../models/lecture.js";
+import { rm } from "fs";
+import { promisify } from "util";
+import fs from "fs";
+import { User } from "../models/user.js";
+
 
 export const createCourse = tryCatch(async(req, res) => {
     const {title, description, category, createdBy, duration, price} = req.body;
@@ -43,5 +48,63 @@ export const addLecture = tryCatch(async(req,res) => {
     res.status(201).json({
         message: "Lecture Added",
         lecture,
+    });
+});
+
+export const deleteLecture = tryCatch(async(req, res,) => {
+    const lecture = await Lecture.findById(req.params.id);
+
+    rm(lecture.video, () => {
+        console.log("Video Deleted");
+    });
+
+    await lecture.deleteOne();
+
+    res.json({ message: "Lecture Deleted"});
+});
+
+
+const unlinkAsync = promisify(fs.unlink);
+
+export const deleteCourse = tryCatch(async(req, res) => {
+    const course = await Course.findById(req.params.id);
+
+    const lectures = await Lecture.find({ course: course._id });
+
+    await Promise.all(
+        lectures.map(async(lecture) => {
+            await unlinkAsync(lecture.video);
+            console.log("Video Deleted");
+        })
+    );
+
+    rm(course.image, () => {
+        console.log("Image Deleted");
+    });
+
+    await Lecture.find({ course: req.params.id }).deleteMany();
+
+    await course.deleteOne();
+
+    await User.updateMany({}, {$pull: {subscription: req.params.id } });
+
+    res.json({
+        message: "Course Deleted",
+    });
+});
+
+export const getAllStats = tryCatch(async(req, res) => {
+    const totalCourses = (await Course.find()).length;
+    const totalLectures = (await Lecture.find()).length;
+    const totalUsers = (await User.find()).length;
+
+    const stats = {
+        totalCourses,
+        totalLectures,
+        totalUsers,
+    };
+
+    res.json({
+        stats,
     });
 });
